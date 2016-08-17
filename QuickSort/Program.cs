71d3bacff2a55c07f19e7aebed217e6f20b5195c
Priyanshu Agrawal <priyanshuagrawal@outlook.com>
2016-08-17 22:40:49 +0530

    Added Quick sort logic

--- QuickSort/Program.cs
+++ QuickSort/Program.cs
@@ -10,15 +10,47 @@ namespace QuickSort
     {
         static void Main(string[] args)
         {
-            int n = 7;
             int[] a = new int[] { 2, 4, 1, 6, 5, 3, 7 };
-            
-                        
 
-            for (int i = 0; i < n; i++)
+            QuickSort(a, 0, a.Length - 1);
+
+            for (int i = 0; i < a.Length; i++)
             {
                 Console.Write(a[i] + " ");
             }
+        }
+
+        static int Partition(int[] a, int start, int end)
+        {
+            int pivot = a[end];
+            int partitionIndex = start;
+            for (int i = start; i < end; i++)
+            {
+                if (a[i] <= pivot)
+                {
+                    swap(ref a[i], ref a[partitionIndex]);
+                    partitionIndex++;
+                }
+            }
+            swap(ref a[partitionIndex], ref a[end]);
+            return partitionIndex;
+        }
+
+        static void QuickSort(int[] a, int start, int end)
+        {
+            if (start < end)
+            {
+                int partitionIndex = Partition(a, start, end);
+                QuickSort(a, start, partitionIndex - 1);
+                QuickSort(a, partitionIndex + 1, end);
+            }
+        }
+
+        private static void swap(ref int v1, ref int v2)
+        {
+            int temp = v1;
+            v1 = v2;
+            v2 = temp;
         }
     }
 }
